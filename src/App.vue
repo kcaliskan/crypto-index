02eb67ca3fd5980f31f65634238ac7a0a8539eae
Kaan Caliskan <kaancaliskan@Kaans-MacBook-Pro.local>
2022-01-21 00:52:01 +0300

    home component added

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,27 @@
 <template>
-  <Header />
+  <div>
+    <Header />
+    <Home />
+  </div>
 </template>
 
 <script>
 import Header from "./components/Header";
+import Home from "./views/Home";
 
 export default {
   name: "App",
   components: {
     Header,
+    Home
   },
 };
 </script>
 
 <style lang="scss">
 @import "bootstrap";
+
+body{
+  background-color: $gray-100;
+}
 </style>
