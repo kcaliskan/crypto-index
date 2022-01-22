6ea7b8b2529f4b3565fe69e5bb0de3dfdc778edc
Kaan Caliskan <kaancaliskan@Kaans-MacBook-Pro.local>
2022-01-22 21:02:57 +0300

    crypto store, cryptocurrencies api request and loading message structure

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+module.exports = {
+  CRYPTO_API: {
+    BASE_URL:"https://pacific-waters-19946.herokuapp.com",
+    CRYPTO_PATH: "/v2/assets",
+    EXCHANGE_PATH: "/v2/exchanges"
+  }
+}
--- src/views/TrendingListings.vue
+++ src/views/TrendingListings.vue
@@ -1,9 +1,25 @@
 <template>
-  <div>Trending Listings</div>
+  <div>
+    <div v-if="showLoading">Loading</div>
+    <div v-else>Crypto data: {{cryptoValues}}</div>
+  </div>
 </template>
 
 <script>
+import { mapActions, mapGetters } from "vuex";
+
 export default {
   name: "TrendingListings",
+  methods: {
+    ...mapActions(["fetchCryptoCurrencies"]),
+  },
+  computed: {
+    ...mapGetters(["cryptoValues", "showLoading"]),
+  },
+  beforeMount() {
+    if (!this.cryptoValues.length) {
+      this.fetchCryptoCurrencies();
+    }
+  },
 };
 </script>
